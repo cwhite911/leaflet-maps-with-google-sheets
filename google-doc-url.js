c7e8523222687800cc92afe30b64f2f4c4059b13
cwhite911 <smortopahri@gmail.com>
2019-01-28 11:25:19 -0500

    Updated  google sheet link

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1X6nKdU9w80ltvXLtyxL5iiBsWHKWHCLnct5lBt4lsF8/edit#gid=0';
